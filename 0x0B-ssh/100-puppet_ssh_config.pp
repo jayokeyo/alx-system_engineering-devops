2adae5c6264ee40e46921068fa3d9149b54a0124
Julius-Okeyo <jaykopiyo@gmail.com>
2023-04-23 15:47:52 -0700

    Updated 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 # Ruby script to manage ssh_config file using Puppet
-file_line {'PasswordAuthentication no': 
+file_line {'PasswordAuthentication': 
       ensure => 'present',
-      path => '~/etc/ssh/ssh_config'
+      path => '~/etc/ssh/ssh_config',
+      line => 'PasswordAuthentication no'
 }
-file_line {'IdentityFile ~/.ssh/school':
+file_line {'IdentityFile':
       ensure => 'present', 
       path => '~/etc/ssh/ssh_config'
+      line => 'IdentityFile ~/.ssh/school'
 }
