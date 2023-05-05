684f8a11f5cc262e49c085e923e4fb8a11391a49
Julius Okeyo <80482593+Julius-Okeyo@users.noreply.github.com>
2023-05-05 15:34:35 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Create a file /tmp/school if doesn't exist and apply attributes
-file { '/tmp/school':
-  ensure  => 'present',
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
-}
--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# Install flask using pip3 if not installed
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
-}
