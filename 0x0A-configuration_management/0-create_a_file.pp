e7bba3ae9a7bf84bdd82de5b43238074db6259dd
Julius-Okeyo <jaykopiyo@gmail.com>
2023-06-23 11:07:40 -0700

    Updated 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-file {school:
-  ensure => 'present',
+#Creates a file in /tmp/school
+
+file {'school':
+  ensure  => 'present',
   content => 'I love Puppet',
   mode    => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  path => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  path    => '/tmp/school',
 }
