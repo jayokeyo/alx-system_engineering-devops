0e2f978b6c732a282ad2ad61a0036279d4f9e2ef
Julius-Okeyo <jaykopiyo@gmail.com>
2023-06-25 06:23:37 -0700

    Updated 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,13 @@
 # Automates ssh_config configuration
 include stdlib
 file_line { 'PasswordAuthentication':
-ensure  => present,
+ensure  => 'present',
 path    => 'etc/ssh/ssh_config',
 line    => 'PasswordAuthentication no',
-replace => true,
 }
 
 file_line { 'IdentityFile':
-ensure  => present,
+ensure  => 'present',
 path    => '/etc/ssh/ssh_config',
 line    => '     IdentityFile ~/.ssh/school',
-replace => true,
 }
