7eb5d1c9311c8fd170405fa93a165aa70adcddfe
Julius-Okeyo <jaykopiyo@gmail.com>
2023-08-09 10:20:53 -0700

    Added 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Corrects typo in /var/www/html/wp-settings.php
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
