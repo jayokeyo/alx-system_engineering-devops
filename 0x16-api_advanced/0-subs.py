71aa0a01f1e740e52037d311e857f940fc9642c1
Julius-Okeyo <jaykopiyo@gmail.com>
2023-08-10 09:18:06 -0700

    Added 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Query reddit api for number of subscribers on a given subreddit."""
+import requests
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+            "User-Agent": "Linux Ubuntu 20.04"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
