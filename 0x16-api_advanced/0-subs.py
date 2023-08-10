25786a188b9c36a385a5905be497a9239535f03a
Julius-Okeyo <jaykopiyo@gmail.com>
2023-08-10 09:27:39 -0700

    Trying to fix PEP8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,9 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Linux Ubuntu 20.04"}
+    headers = {
+        "User-Agent": "Linux:Ubuntu20.04"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
